b5f53a6dbbf65471e7ab4e493b97f549b7e6ca0e
mariia.onuschuk <marya.vadymovna@gmail.com>
2023-08-20 21:08:08 +0300

    formatted configs prettier rules added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-	content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-			colors: {
-					transparent: 'transparent',
-					current: 'currentColor',
-					black: colors.black,
-					white: colors.white,
-					emerald: colors.emerald,
-					indigo: colors.indigo,
-					yellow: colors.yellow,
-					stone: colors.stone,
-					sky: colors.sky,
-					blue: colors.blue,
-					cayan: colors.cayan,
-					neutral: colors.trueGray,
-					gray: colors.coolGray,
-					slate: colors.blueGray,
-			},
-		},
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        stone: colors.stone,
+        sky: colors.sky,
+        blue: colors.blue,
+        cayan: colors.cayan,
+        neutral: colors.trueGray,
+        gray: colors.coolGray,
+        slate: colors.blueGray,
+      },
+    },
   },
-	safelist: [
-    'bg-dark-50',
-    'bg-light-blue-300',
-  ],
+  safelist: ["bg-dark-50", "bg-light-blue-300"],
   plugins: [],
-}
-
+};
