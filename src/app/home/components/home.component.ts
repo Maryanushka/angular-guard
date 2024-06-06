b1ac88b2a6ae44c30b87b212473a6833f4ac118c
Mariia <marya.vadymovna@gmail.com>
2024-06-06 12:56:16 +0300

    social media component added , home in progress

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router } from '@angular/router';
 import { Subject, filter, take, takeUntil } from 'rxjs';
 import { NavigationModule } from '../../shared/components/navigation/navigation.module';
+import { SocialMediaComponent } from '../../shared/components/social-media/social-media.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavigationModule],
+  imports: [CommonModule, NavigationModule, SocialMediaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/app/shared/components/social-media/social-media.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-social-media',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './social-media.component.html',
+  styleUrl: './social-media.component.scss',
+})
+export class SocialMediaComponent {}
